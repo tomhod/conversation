db35dba2cea52d7217d8911ae7aee64c466d2b7c
tomhod <107213226+tomhod@users.noreply.github.com>
2022-09-16 12:47:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+  <link rel="stylesheet" href="istyle.css">
+</head>
+<body>
+
+
+<div class="container-fluid">
+
+
+
+
+
+
+<div class="bg-info p-3 text-center">
+    <p>All rights resered © -- Designed by THOMAS CYRIL</p>
+</div>
+
+</div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,229 @@
+
+    function m(){
+    var m = document.getElementById('m');
+    m.innerHTML = "hey ";
+    
+    }
+    
+    
+    function f()
+    {
+    
+       var y= document.getElementById('f')
+        y.innerHTML = 'hello';
+   
+       
+    
+    if(y.innerHTML === 'hello'){
+         setTimeout(k, 4000)
+      function k(){
+       var p= document.getElementById('m');
+        p.innerHTML= 'HEY';
+
+        if( p.innerHTML=== 'HEY'){
+            setTimeout(s, 6000)
+            function s()
+            {
+            var g= document.getElementById('f');
+            g.innerHTML = "how are you";
+
+            if(g.innerHTML === 'how are you'){
+                setTimeout(k, 3000)
+             function k(){
+              var p= document.getElementById('m');
+               p.innerHTML= 'i am fine and u';
+
+               if(  p.innerHTML=== 'i am fine and u'){
+                setTimeout(s, 3000)
+                function s()
+                {
+                var g= document.getElementById('f');
+                g.innerHTML = "i'm good";
+
+                if(g.innerHTML === "i'm good"){
+                    setTimeout(k, 3000)
+                 function k(){
+                  var p= document.getElementById('m');
+                   p.innerHTML= 'nice  rolling hair';
+
+                   if(  p.innerHTML= 'nice  rolling hair'){
+                    setTimeout(s, 4000)
+                    function s()
+                    {
+                    var g= document.getElementById('f');
+                    g.innerHTML = "Thank you";
+
+                    if(g.innerHTML = "Thank you"){
+                        setTimeout(k, 4000)
+                     function k(){
+                      var p= document.getElementById('m');
+                       p.innerHTML= 'Are you from kerla';
+
+                       if(  p.innerHTML= 'Are you from kerla'){
+                        setTimeout(s, 4000)
+                        function s()
+                        {
+                        var g= document.getElementById('f');
+                        g.innerHTML = "Yes..";
+
+                        if(   g.innerHTML = "Yes.."){
+                            setTimeout(s, 4000)
+                            function s()
+                            {
+                            var g= document.getElementById('f');
+                            g.innerHTML = "and you";
+
+                            if( g.innerHTML = "and you"){
+                                setTimeout(k, 4000)
+                             function k(){
+                              var p= document.getElementById('m');
+                               p.innerHTML= 'i stay where u stay';
+
+                               if( p.innerHTML= 'i stay where u stay'){
+                                setTimeout(k, 4000)
+                             function k(){
+                              var p= document.getElementById('m');
+                               p.innerHTML= 'i like the photo with red dress';
+
+                              
+
+                                if( p.innerHTML= 'i like the photo with red dress'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'tysm';
+
+                                   if(p.innerHTML= 'tysm'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'are u studied in --  this college';
+
+                                  if(wp.innerHTML= 'are u studied in --  this college'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'yes....';
+
+                                   if( p.innerHTML= 'yes....'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'but i haventh seen you in the college';
+     
+                                   
+                                  if(wp.innerHTML= 'but i haventh seen you in the college'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'hmm....';
+
+                                   if(  p.innerHTML= 'hmm....'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'have u seen me';
+
+                                   
+                                  if( wp.innerHTML= 'have u seen me'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'yes';
+
+                                   if(  p.innerHTML= 'yes'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'ok where are you now';
+
+                                  if(wp.innerHTML= 'ok where are you now'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'i am riding horse';
+
+                                   
+                                   if(  p.innerHTML= 'i am riding horse'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'what';
+
+                                  if( wp.innerHTML= 'what'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'due to covid i am afraid so did"nt get train "<br>" ticket so i"m coming by riding a horse ';
+
+                                   if( p.innerHTML= 'due to covid i am afraid so did"nt get train "<br>" ticket so i"m coming by riding a horse '){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'ooh';
+     
+                                  if(wp.innerHTML= 'ooh'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'and what are you doing';
+
+                                   if( p.innerHTML= 'and what are you doing'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'Texting you';
+
+                                  if( wp.innerHTML= 'Texting you'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'oh';
+
+                                   if( p.innerHTML= 'oh'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'what is ur favorite vegetable';
+
+                                  if(wp.innerHTML= 'what is ur favorite vegetable'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var p= document.getElementById('f');
+                                   p.innerHTML= 'aalu';
+
+                                   if( p.innerHTML= 'aalu'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'ooooh ';
+
+                                  if(  wp.innerHTML= 'ooooh '){
+                                    setTimeout(k, 5000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'ok gn bye';
+
+                                  if(  wp.innerHTML= 'ok gn bye'){
+                                    setTimeout(k, 4000)
+                                 function k(){
+                                  var wp= document.getElementById('m');
+                                  wp.innerHTML= 'bye. ';
+ 
+    
+
+                               
+                        
+                        
+
+            }}}}}}}}}}}}}}}}}
+        }
+      }}}}}
+    
+                               }}
+
+    }}
+
+    }
+      }}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+   }}
